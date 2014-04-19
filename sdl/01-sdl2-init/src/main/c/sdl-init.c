cc9f35d783733f594a44530d002b6c877fdd411a
Juraj Michalek <juraj.michalek@sinusgear.com>
2014-04-19 12:01:24 +0200

    use gradle to build example

---
+++ sdl/01-sdl2-init/src/main/c/sdl-init.c
@@ -0,0 +1,11 @@
+// sdl2-sample.cpp : SDL2 example - initialize and quit
+//
+
+#include "SDL.h"
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);	
+    SDL_Quit();
+    return 0;
+}
+
